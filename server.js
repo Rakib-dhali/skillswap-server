d01f9b5cd3af35cdec3bb1ecf8f057c301ceb501
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-06-23 10:14:42 +0600

    feat: implement MongoDB connection and environment configuration with .env file ignored

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("skillswap");
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-    res.send("Skillswap Server is running!")
-})
+  res.send("Skillswap Server is running!");
+});
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
+  console.log("Server is running on port 5000");
+});
